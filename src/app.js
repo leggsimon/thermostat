5fae454bcbeccca5bc96e08e6225138a673d86d8
Simon Legg <leggsimon@gmail.com>
2015-07-10 09:59:06 +0100

    Thermostat now uses OpenWeatherMap API to get city weathers + Some CSS added

--- src/app.js
+++ src/app.js
@@ -27,9 +27,26 @@ var resetThermostat = function() {
   temperatureRefresh();
 }
 
+var getCity = function () {
+  return $('#cityList').val()
+};
+
+var getCityTemperature = function(city) {
+  console.log(city)
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=b3e354ab2227f3e98542190461b7fe44&units=metric"
+  var response = $.get(url).done(function() {
+    $('#cityWeather').html("Current temperature: " + response.responseJSON.main.temp + "&#x2103")
+  })
+
+}
+
 temperatureRefresh();
+getCityTemperature(getCity());
 
 $('#upButton').click(increaseTemperature);
 $('#downButton').click(decreaseTemperature);
 $('#powersaver').change(togglePowerSaverMode);
 $('#resetButton').click(resetThermostat);
+$('#cityList').on('change', function() {
+    getCityTemperature(getCity())
+});
